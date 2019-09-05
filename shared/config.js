6c82fdff0d621d0e1f4f67e1df812c6038011130
Hugo Girard <Hugo.Girard@equisoft.com>
2019-09-05 14:42:51 -0400

    add aks file

--- shared/config.js
+++ shared/config.js
@@ -4,7 +4,10 @@ module.exports = class Configuration {
     constructor(){
         // Load environment variable when
         // running in development
-        require('dotenv').config();
+        if (process.env.NODE_ENV !== "production") {
+            require('dotenv').config();
+        }
+        
         this.port = process.env.PORT || 5000;
         this.sqlCnxString = process.env.SQL_CNX_STRING;
         this.machineName = process.env.MACHINE_NAME;
